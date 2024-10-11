a0a368770eda5a6c84096b75944e1ce618a4aeaf
F1 <1@1.ru>
2024-10-11 22:10:32 +0300

    11.10

---
+++ main.py
@@ -0,0 +1,12 @@
+print((9**0.5)*5)   # 1st program
+
+print(9.99 > 9.98 and 1000 != 1000.1) # 2nd program
+
+print(2*2+2)# 3rd  program
+print((2+2)*2)
+print(2*2+2 == 2*(2+2))
+
+x = ((float('123.456'))*10) #4th program
+print(x)
+s =int(x) % 10
+print(s)
